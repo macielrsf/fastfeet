222469a710cbc10b880d870f6772f4aa131926af
Hubsoft <macielrsf@gmail.com>
2020-05-02 12:35:31 -0300

    added new tables

---
+++ api/src/database/migrations/20200502153005-add-avatar-field-to-deliveryman.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn(
+            'deliveryman',
+            'avatar_id', {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+                references: { model: 'files', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+            },
+        )
+    },
+
+    down: (queryInterface) => {
+        return queryInterface.removeColumn('deliveryman', 'avatar_id');
+    }
+};
